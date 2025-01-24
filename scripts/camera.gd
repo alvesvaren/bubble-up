e42d74d13dae98d331201cb59136510abe5aa3d3
B1nus <119787571+B1nus@users.noreply.github.com>
2025-01-24 21:34:16 +0100

    Camera follows path

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -10,8 +10,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var avarage_pos = Vector2(0,0)
 	var count = 0
-	for child in get_parent().get_children():
-		if "Player" == child.name[0:6]:
+	var progress = {}
+	for child in get_parent().get_parent().get_parent().get_children():
+		if child.name.find("Player") != -1:
+			progress[child.name] = get_parent().get_parent().curve.get_closest_offset(child.global_position)
 			avarage_pos += child.global_position
 			count += 1
-	global_position = avarage_pos
+			
+	get_parent().progress = progress[first_player(progress)]
+
+func first_player(progress) -> String:
+	var best = "Player"
+	for player in progress:
+		if progress[player] > progress[best]:
+			best = player
+	return best
