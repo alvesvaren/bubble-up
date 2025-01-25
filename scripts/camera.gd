4c9485e29004de95d60de9cee1a3154dd13c259d
B1nus <123@1>
2025-01-25 13:41:47 +0100

    Camera fix and wall

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -2,6 +2,7 @@ extends Camera2D
 
 
 const CAUSTIC_OFFSET_SCALE = 0.1
+const FIRST_PLAYER_OFFSET = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,9 +20,20 @@ func _process(delta: float) -> void:
 			avarage_pos += child.global_position
 			count += 1
 
-		$"..".progress = progress[first_player(progress)]
+		var first = first_player(progress)
+		var first_edge_dist = distance_to_edge($"../../../players".get_node(first).global_position)
+		$"..".progress += delta * (3.5 - first_edge_dist / 200) * (max(0, progress[first_player(progress)] - FIRST_PLAYER_OFFSET) - $"..".progress)
 		$shader.global_position = get_screen_center_position()
 		$shader.material.set_shader_parameter("offset", get_screen_center_position() * CAUSTIC_OFFSET_SCALE)
+
+
+func distance_to_edge(position: Vector2) -> float:
+	var distance_to_top = position.y - (get_screen_center_position().y - get_viewport_rect().size.y / 2)
+	var distance_to_bottom = (get_screen_center_position().y +  get_viewport_rect().size.y / 2) - position.y
+	var distance_to_left = position.x - (get_screen_center_position().x - get_viewport_rect().size.x / 2)
+	var distance_to_right = (get_screen_center_position().x +  get_viewport_rect().size.x / 2) - position.x
+	
+	return min(distance_to_left, distance_to_right, distance_to_bottom, distance_to_top)
 
 func first_player(progress) -> String:
 	var best = "Player"
